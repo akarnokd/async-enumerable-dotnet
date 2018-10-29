3c29536f01cb3da866d1c434beb1e4ae2d08e497
akarnokd <akarnokd@gmail.com>
2018-10-29 15:29:14 +0100

    Add test for CancellationHelper

---
+++ async-enumerable-dotnet-test/CancellationHelperTest.cs
@@ -0,0 +1,20 @@
+// Copyright (c) David Karnok & Contributors.
+// Licensed under the Apache 2.0 License.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using Xunit;
+using async_enumerable_dotnet;
+using System.Threading.Tasks;
+
+namespace async_enumerable_dotnet_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async void Test1()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
